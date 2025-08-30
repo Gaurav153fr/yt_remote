f46363038e44cec06f92e26ca57c76e66ee6c8f8
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2025-08-30 15:04:23 +0530

    added path

--- api/server.js
+++ api/server.js
@@ -17,6 +17,7 @@ const io = socketIo(server, {
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.set("views", path.join(__dirname, "views"));
 // In-memory room storage
 let rooms = {}; // { roomCode: { members: [socketId,...] } }
 
